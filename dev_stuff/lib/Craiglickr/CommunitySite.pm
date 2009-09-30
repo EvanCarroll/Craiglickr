2b4d4907bc0d71fdefd4581fb05b0de8cf902b16
Evan Carroll <me@evancarroll.com>
2009-09-30 18:58:14 -0500

    now works with metropolitan areas (a form a sub-craigslist such that the parent is not post-able)

--- dev_stuff/lib/Craiglickr/CommunitySite.pm
+++ dev_stuff/lib/Craiglickr/CommunitySite.pm
@@ -13,7 +13,7 @@ has 'uri_orig' => ( isa => 'URI' , is => 'ro' , required => 1 );
 
 has 'description' => ( isa => 'Str' , is => 'ro' );
 
-## id (hun) is used for posting: https://post.craigslist.org/hum
+## id (hum) is used for posting: https://post.craigslist.org/hum
 has 'posting_uid' => (
 	isa => 'Maybe[Str]'
 	, is => 'ro'
@@ -21,17 +21,19 @@ has 'posting_uid' => (
 	, default => sub {
 		my $self = shift;
 	
-		return undef if $self->subsites;
-	
-		my $uri = URI->new(
-			$self->_response_root
-			->look_down( id => 'postlks' )
-			->look_down( _tag => 'a', href => qr/post\./ )
-			->attr( 'href' )
-		);
-		my $path = URI->new($uri)->path;
-		substr($path,0,1,'');
-		$path;
+##		use Digest::MD5 'md5_hex';
+##		my $md5 = md5_hex( $self->_resp->content );
+##		open ( my $fh, '>', "out/" . time() . "_$md5.html" ) or die;
+##		print $fh $self->_resp->content;
+		
+		my $uid;
+		if ( my $postlks = $self->_response_root->look_down( id => 'postlks' ) ) {
+			my $href = $postlks->look_down( _tag => 'a', href => qr/post\./ )->attr( 'href' );
+			my $path = URI->new($href)->path;
+			substr($path,0,1,'');
+			$uid = $path;
+		}
+		$uid
 	}
 );
 
@@ -62,29 +64,64 @@ has '_response_root' => (
 );
 
 has 'subsites' => (
-	isa         => 'ArrayRef[Craiglickr::CommunitySite] | Undef'
+	isa         => 'ArrayRef[Craiglickr::CommunitySite]'
 	, is        => 'ro'
 	, metaclass => 'Collection::Array'
-	, provides  => { 'push' => 'push_subsite' }
+	, provides  => { 'push' => 'push_subsite', 'empty' => 'has_subsites' }
 	, lazy      => 1
+	, predicate => 'preset_subsites'
 	, default   => sub {
 		my $self = shift;
 
-		my $list = $self->_response_root->look_down( _tag => 'div', id => 'list' );
+		my ( $list, @subsites );
+		if ( $list = $self->_response_root->look_down( _tag => 'div', id => 'list' ) ) {
+			
+			my @links = $list->look_down( _tag => 'a' );
+			
+			warn sprintf ("SS: Dedicated-list variant %s added for %s\n", scalar @links, $self->uri_dest);
+			
+			foreach my $element ( @links ) {
+				push @subsites, $self->meta->new_object({
+					name        => $element->as_trimmed_text
+					, uri_orig  => URI->new( $element->attr('href') )
+				});
+			}
 		
-		return undef unless $list;
-
-		my @links = $list->look_down( _tag => 'a' );
-		
-		my @subsites;
-		foreach my $element ( @links ) {
-			push @subsites, $self->meta->new_object({
-				name        => $element->as_trimmed_text
-				, uri_orig  => URI->new( $element->attr('href') )
-			});
 		}
+		
+		## Lets get those sub-craiglists too, http://dallas.craigslist.org/ftw
+		elsif ( $list = $self->_response_root->look_down( _tag => 'span', class => 'for' ) ) {
+			## Stop infinate recursion with subsites
+			## breadcrumb thing on a subsite, points to other subsites, which point back, etc.
+			return [] if $self->uri_orig->path =~ /\w{3,}/;
+		
+			my @links = $list->look_down( _tag => 'a' );
 	
+			warn sprintf ("SS: Sector variant %s added for %s\n", scalar @links, $self->uri_dest);
+
+			foreach my $element ( @links ) {
+				my $name = $element->attr('href');
+				$name =~ tr'\/''d;
+
+				my $uri = URI->new( $self->uri_dest );
+				$uri->path( $element->attr('href') );
+				use XXX; YYY [{
+					posting_uid => $self->posting_uid . "/$name"
+					, uri_orig  => $uri
+					, name      => $element->attr('title')
+				}];
+
+				push @subsites, $self->meta->new_object({
+					posting_uid => $self->posting_uid . "/$name"
+					, uri_orig  => $uri
+					, name      => $element->attr('title')
+				});
+			}
+			
+		}
+
 		\@subsites;
+
 	}
 );
 
